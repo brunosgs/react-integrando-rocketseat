762a5bf486aa6cc7082e41430a95a417cc255d6f
brunosgs <brunosoges@gmail.com>
2025-08-31 18:31:04 -0300

    adição de rotas
    feat: configurado para existir roteamento para as páginas da aplicação.

---
+++ pizzashop-web/src/routes.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter } from 'react-router'
+import { Dashboard } from './pages/app/dashboard'
+import { SingIn } from './pages/auth/sign-in'
+
+export const router = createBrowserRouter([
+   {
+      path: '/',
+      element: <Dashboard />
+   },
+   {
+      path: 'sing-in',
+      element: <SingIn />
+   }
+])
